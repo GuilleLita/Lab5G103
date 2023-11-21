44848b44d1780a65e09fa27db6977b8aadcf3e50
Guillermo <guilleloscertales375@gmail.com>
2023-11-21 14:05:04 +0000

    Add Hallway frontend "casi" terminado

--- frontend/src/AddHallway.tsx
+++ frontend/src/AddHallway.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import AsyncSelect, { useAsync } from 'react-select/async';
+import  AsyncSelect, { useAsync } from 'react-select/async';
+import  ReactSelect from 'react-select';
+import { useState } from 'react';
+import {useRef} from 'react';
+import config from './config'
 
 
 import './AddHallway.css';
 import { get } from 'http';
+import Input from 'react-select/dist/declarations/src/components/Input';
 
 const fetchBuildings = async  ()  => {
     var buildings: any[] =  []
-    const res = await fetch('http://localhost:4000/api/building/getall');
+    const res = await fetch(config.ServerURL + '/api/building/getall');
     const data = await res.json();
     for (let i = 0; i < data.buildingDTO.length; i++) {
         buildings.push({ value: data.buildingDTO[i].buildingName, label: data.buildingDTO[i].buildingName });
@@ -19,16 +24,26 @@ const fetchBuildings = async  ()  => {
 
 
 function AddHallway() {
-    
+    const [floor1Options, setFloor1Options] = useState<any[]>([]);
+    const [floor2Options, setFloor2Options] = useState<any[]>([]);
+
+    const [selected, setSelected] = useState({ value: '', label: ''});
+    const [selected2, setSelected2] = useState({ value: '', label: ''});
+    const [floorSelected, setFloorSelect] = useState({ value: '', label: ''});
+    const [floorSelected2, setFloorSelect2] = useState({ value: '', label: ''});
+    type SelectOptionType = { label: string, value: string }
+
+    const position1 = useRef<any>(null);
+    const position2 = useRef<any>(null);
+
     const getBuildings = async () => {
-        const res = await fetch('http://localhost:4000/api/building/getall');
+        const res = await fetch(config.ServerURL + '/api/building/getall');
         const data = await res.json();
         var retVal: any[] = data.buildingDTO;
         return retVal;
     }
 
-    const buildings = getBuildings();
-
+    
     const customStyles = {
         control: (base:any) => ({
           ...base,
@@ -41,31 +56,100 @@ function AddHallway() {
       };
 
     const searchFloors = async (buildingName: string) => {
-        const res = await getBuildings();
         let floors: any[] = [];
-        for (let i = 0; i < res.length; i++) {
-            if (res[i].buildingName === buildingName) {
-                floors = res[i].floors;
+        console.log(buildingName);
+        let buildings = await getBuildings();
+        for (let i = 0; i < buildings.length; i++) {
+            if (buildings[i].buildingName === buildingName) {
+                floors = buildings[i].floors;
                 break;
             }
         }
+        let retVal: any[] = [];
         for (let i = 0; i < floors.length; i++) {
-            floors[i] = { value: floors[i].floorNumber, label: floors[i].floorNumber };
+            retVal.push({ value: floors[i], label: floors[i] });
+            
         }
-        return floors;
+        
+        return retVal;
     }
 
     const promiseOptions = () =>
         new Promise<any[]>((resolve) => {  
+            
             resolve(fetchBuildings());
         }
     );
 
-    const promiseOptions2 = (floor :string) =>
-        new Promise<any[]>((resolve) => {  
-            resolve(searchFloors(floor));
+    const handleInputChange =async (option: SelectOptionType | null) => {
+        if (option) {
+            
+            setSelected(option)
+            setFloor1Options(await searchFloors(option.value));
+          }
+    }
+
+    const handleInputChange2 =async (option: SelectOptionType | null) => {
+        if (option) {
+            
+            setSelected2(option)
+            setFloor2Options(await searchFloors(option.value));
+          }
+    }
+
+    const handleFloorInputChange2 =async (option: SelectOptionType | null) => {
+        if (option) {
+            setFloorSelect2(option)
+          }
+    }
+
+    const handleFloorInputChange =async (option: SelectOptionType | null) => {
+        if (option) {
+            setFloorSelect(option)
+          }
+    }
+
+    const getCodes = async (building1: string, building2: string) => {
+        let buildings = await getBuildings();
+        let retVal: string[] = []
+        for(let i = 0; i < buildings.length; i++ ){
+            if(buildings[i].buildingName === building1 || buildings[i].buildingName === building2){
+                retVal.push(buildings[i].buildingCode)
+            }
         }
-    );
+        return retVal
+    }
+
+    const OnClickListner = async () =>{
+        let buildingCodes: string[] = await getCodes(selected.value, selected2.value)
+        if (position1.current !== null && position2.current !== null) {
+            let input1 = position1.current.value
+            let input2 = position2.current.value
+        
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ buildingsCode: buildingCodes, floorsId: [floorSelected.value, floorSelected2.value], position: [input1, input2] })
+        };
+        fetch( config.ServerURL +'/api/hallway/create', requestOptions)
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                const data = isJson && await response.json();
+    
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    const error = (data && data.message) || response.status;
+                    return Promise.reject(error);
+                }
+                
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+        }
+    }
+
 
     return (
         <div className="Hallway-body">
@@ -73,19 +157,25 @@ function AddHallway() {
             <div className="Hallway-Form">
                 <div className="Hallway-Form-Field">
                     <label htmlFor="building1">Building 1</label>
-                    <AsyncSelect styles={customStyles} isSearchable={false} cacheOptions defaultOptions loadOptions={promiseOptions} />
+                    <AsyncSelect styles={customStyles} isSearchable={false} cacheOptions defaultOptions loadOptions={promiseOptions} onChange={handleInputChange} />
                     <label htmlFor="floor1">Floor</label>
-                    <AsyncSelect styles={customStyles} isSearchable={false} cacheOptions defaultOptions loadOptions={promiseOptions2} />
+                    <ReactSelect styles={customStyles} isSearchable={false} options={floor1Options} onChange={handleFloorInputChange}/>
                 </div>
                 <div className="Hallway-Form-Field">
                     <label htmlFor="building2">Building 2</label>
-                    <AsyncSelect styles={customStyles} isSearchable={false} cacheOptions defaultOptions loadOptions={promiseOptions} />
+                    <AsyncSelect styles={customStyles} isSearchable={false} cacheOptions defaultOptions loadOptions={promiseOptions} onChange={handleInputChange2} />
                     <label htmlFor="floor2">Floor</label>
-                    <AsyncSelect styles={customStyles} isSearchable={false} cacheOptions defaultOptions loadOptions={promiseOptions2} />
+                    <ReactSelect styles={customStyles} isSearchable={false} options={floor2Options} onChange={handleFloorInputChange2} />
                 </div>
                 
             </div>
-            <button className="submit" >Create Hallway</button>
+            <div className='PositionForm'>
+                <label htmlFor="position">Position in bulilding 1:  </label>
+                <input ref={position1} type="text" id="position1" name="name" className='input' />
+                <input ref={position2} type="text" id="position2" name="name" className='input' />
+
+            </div>
+            <button className="submit" onClick={OnClickListner} >Create Hallway</button>
         </div>
 
     );
