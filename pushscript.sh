b3a2781cf8fe5999adb19d486baf88adb4fc4862
Guillermo <guilleloscertales375@gmail.com>
2023-12-19 21:23:03 +0000

    arreglado pushscript

--- pushscript.sh
+++ pushscript.sh
@@ -9,10 +9,10 @@ FRON_NAME='Frontend'
 FILERET='/tmp/screenreturn'
 
 #First check git main branch
-#git checkout main &&
-#git fetch --all &&
-#git reset --hard origin/main &&
-#git pull origin main
+git checkout main &&
+git fetch --all &&
+git reset --hard origin/main &&
+git pull origin main
 
 #Then kill screen process
 screen -S ${BACK_NAME} -X quit
