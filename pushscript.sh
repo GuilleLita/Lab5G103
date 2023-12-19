06dcedd2ee33352ed22132c348b61e2cceae996f
Guillermo <guilleloscertales375@gmail.com>
2023-12-19 21:22:19 +0000

    añadido script

---
+++ pushscript.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BACK_DIR="./Bakend"
+FRON_DIR="./frontend"
+
+BACK_NAME='Backend'
+FRON_NAME='Frontend'
+
+FILERET='/tmp/screenreturn'
+
+#First check git main branch
+#git checkout main &&
+#git fetch --all &&
+#git reset --hard origin/main &&
+#git pull origin main
+
+#Then kill screen process
+screen -S ${BACK_NAME} -X quit
+screen -S ${FRON_NAME} -x quit
+
+
+#Iniciar de nuevo el backend
+cd ${BACK_DIR}
+screen -dmS ${BACK_NAME} npm run start
+
+#Iniciar de nuevo frontend
+cd ..
+cd ${FRON_DIR}
+screen -dmS ${BACK_NAME} npm run start
+
+
+
+
+
